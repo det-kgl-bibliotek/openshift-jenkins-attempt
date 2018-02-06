baaa7bb32962c847253896e00f61097b90c27d36
Asger Askov Blekinge <abr@kb.dk>
2018-02-06 18:55:37 +0100

    removed jenkinsfile

--- 1.createStandaloneJenkins.sh
+++ 1.createStandaloneJenkins.sh
@@ -3,3 +3,6 @@
 oc delete project standalone-jenkins --ignore-not-found=true --now=true
 oc new-project standalone-jenkins
 oc new-app --template=jenkins-ephemeral
+
+#Give the jenkins user the right to use openshift templates
+oc policy add-role-to-user registry-viewer system:serviceaccount:standalone-jenkins:jenkins -n openshift
